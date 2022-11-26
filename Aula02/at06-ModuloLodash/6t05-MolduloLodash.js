38374879d42e03f6a35df6b707dc071d3791e5be
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-11-25 21:06:06 -0300

    aula2

--- Aula02/at06-ModuloLodash/6t05-MolduloLodash.js
+++ Aula02/at06-ModuloLodash/6t05-MolduloLodash.js
@@ -7,35 +7,35 @@
 // instalaçao: npm install --save Lodash
 
 // importação do Lodash
-const _= require('lodash')
+const _ = require("lodash");
 
 // cria um array de numeros
-const numeros = [10,7,14,47,32,3]
-const numeros2 = [100,28,37]
+const numeros = [10, 7, 14, 47, 32, 3];
+const numeros2 = [100, 28, 37];
 
-console.log(numeros)
+console.log(numeros);
 
 // soma os numeros
-console.log('Soma dos números:',_.sumBy(numeros))
+console.log("Soma dos números:", _.sumBy(numeros));
 
 // exibe o primeiro numero do array
-console.log('Primeiro número:',_.first(numeros))
+console.log("Primeiro número:", _.first(numeros));
 // exibe- o ultimo numero
-console.log('Último número:',_.last(numeros))
+console.log("Último número:", _.last(numeros));
 // exibe o menor número
-console.log('Menor número:',_.min(numeros))
+console.log("Menor número:", _.min(numeros));
 // exibe o maior número
-console.log('PMaior número:',_.max(numeros))
+console.log("PMaior número:", _.max(numeros));
 // media dos números
-console.log('Média número:',_.mean(numeros))
+console.log("Média número:", _.mean(numeros));
 // união de  dois arrays
-console.log('União número:',_.union(numeros,numeros2))
+console.log("União número:", _.union(numeros, numeros2));
 
-const numerosMegaSena= ()=>{
-    return Math.trunc(Math.random()*60)
-}
+const numerosMegaSena = () => {
+  return Math.trunc(Math.random() * 60);
+};
 
 // executa a funçao numerosMegaSena 6 vezes
-const jogoCompleto = _.times(6,numerosMegaSena)
+const jogoCompleto = _.times(6, numerosMegaSena);
 
-console.log(jogoCompleto)
+console.log(jogoCompleto);
