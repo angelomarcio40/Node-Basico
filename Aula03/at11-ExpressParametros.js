a36f48ff69eb68a52bd6222fcbb1970f8743ff18
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-11-28 22:27:50 -0300

    Express rotas

--- Aula03/at11-ExpressParametros.js
+++ Aula03/at11-ExpressParametros.js
@@ -11,14 +11,32 @@ app.get('/',(req,res)=>{
 // rota que recebe parametros
 // http://localhost:4500/cadastrar/parametros
 // captura de parametros via GET
-app.get('/cadastrar/:nome',(req,res)=>{
-    // cria a variavel nome e atribui o valor enviado pelo usuario
-    let nome = req.params.nome
-    // cria a varialvel email e captura o valor email
-    let email = req.params.email
-    res.send(req.params.email)
+app.get('/cadastrar/:pagina',(req,res)=>{
+    // ,captura a pagina de cadastro
+    let pagina = req.params.pagina
+    
+    let url = ''
+    let stausCode = 404
+
+    if(pagina == 'usuarios'){
+        statusCode = 200
+        url = '/cadastrar-usuarios.html'
+    }else if(pagina == 'produtos'){
+        statusCode = 200
+        url = '/cadstrar-produtos.html'
+    }else{
+        url = '404.html'
+    }
+
+    // devolve um arquivo
+    res.status(statusCode)
+    res.sendFile(__dirname+url)
+})
+
+app.get('/consultar',(req,res)=>{
+
 })
 
 app.listen(porta,()=>{
-    console.log(`Servidor rodando: http//localhost:${porta}`)
+    console.log(`Servidor rodando: htt://localhost:${porta}`)
 })
