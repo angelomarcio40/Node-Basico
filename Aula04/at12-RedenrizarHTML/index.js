8a153a16713f37cb71ab93cdeaafb494dbd9e74d
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-11-30 22:23:57 -0300

    Rota post iniciado

--- Aula04/at12-RedenrizarHTML/index.js
+++ Aula04/at12-RedenrizarHTML/index.js
@@ -10,6 +10,16 @@ const porta = 3600
 // define a pasta public com conteudo static (CSS,JS,IMG)
 app.use(express.static('views/public'));
 
+// middlware
+// rota que toda requisição passa por ela
+// rota padrão
+
+// decodifica os parametros enviados para rota
+app.use(express.urlencoded({extende: true}))
+
+// converteos valores para formato JSON
+app.use(express.json())
+
 // criar as rotas
 app.get('/',(req,res)=>{
     res.status(200)
@@ -25,6 +35,14 @@ app.get('/cadastrar',(req,res)=>{
 app.get('/consultar',(req,res)=>{
     res.status(200)
     res.send('<h1>Consultar</h1>')
+})
+
+// cria a rota para cadastrar login
+app.post('/cadastrar/login',(req,res)=>{
+    // let nome = req.body.nome
+
+    let{nome,email,senha,confirmar} = req.body
+    res.send(`Nome:${nome} Email:${email}`)
 })
 
 // rota para retorno de pagina de erro
