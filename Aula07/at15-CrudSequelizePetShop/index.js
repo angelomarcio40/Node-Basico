4dd015f28d535e8cb548a9a357c1133bff63e278
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2023-01-17 19:32:08 -0300

    Crude at 15

---
+++ Aula07/at15-CrudSequelizePetShop/index.js
@@ -0,0 +1,39 @@
+// importa o modulo do express
+const express = require('express')
+
+// inicializa o express
+const app = express()
+
+// importa o modulo do usuario
+const usuario = new require("./model/usuario")
+
+const porta = 5000;
+
+// define a pasta publica que armazena o conteudo estatico(CSS,JS,IMG)
+app.use(express.static('views/public'))
+
+
+// Cria a rota padrao /
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/views/index.html')
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+'/views/login.html')
+})
+
+app.get('/cadastrar',(req,res)=>{
+    res.sendFile(__dirname+'/views/cadastrar.html')
+})
+
+
+app.listen(porta,()=>{
+    console.log(`Servidor rodando em: http://localhost:${porta}`)
+})
+
+
+
+
+
+
+
